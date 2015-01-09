ef2dd932a96185af9d5177a313607d71acbbca7e
Keith Brinks <keithbrinks@me.com>
2015-01-09 16:50:25 -0500

    Remove TEST_VAR

--- language/en/app_join_submitted_lang.php
+++ language/en/app_join_submitted_lang.php
@@ -18,6 +18,5 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
   'PAGE_TITLE' => 'Thank you!',
-  'TEST_VAR' => 'Test',
   'SUBMITTED_TEXT' => 'Thank you for submitting your application!',
 ));
