eb0f0dcd01b23653c6c5859944d620b65172756f
Keith Brinks <keithbrinks@me.com>
2015-11-19 23:50:05 -0600

    CC/ACC join form

--- language/en/join_lang.php
+++ language/en/join_lang.php
@@ -18,9 +18,44 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 
-  'PAGE_TITLE'      => 'Application to Join The Christian Crew',
+  'PAGE_TITLE'                     => 'Application to Join Christian Crew Gaming',
 
-  'INGAME_NAME'     => 'In-Game Name',
-  'REAL_NAME'       => 'Real Name'
+  'APP_TYPE'                       => 'Select if you are joining CC or ACC',
+  'APP_TYPE_CC'                    => 'CC',
+  'APP_TYPE_ACC'                   => 'ACC',
+
+  'PERSONAL_INFORMATION'           => 'Personal Information',
+  'INGAME_NAME'                    => 'In-Game Name',
+  'REAL_NAME'                      => 'Real Name',
+  'REAL_NAME_HELP_TEXT'            => 'First &amp; last',
+  'AGE'                            => 'Age',
+  'LOCATION'                       => 'Location',
+  'LOCATION_HELP_TEXT'             => 'City, State',
+  'STEAM_COMMUNITY_LINK'           => 'Steam Community Link',
+  'STEAM_COMMUNITY_LINK_HELP_TEXT' => 'e.g. http://steamcommunity.com/id/username',
+  'FAVORITE_FISH_SERVERS'          => 'Favorite Fish Server(s)',
+  'CSGO'                           => 'Counter-Strike: Global Offensive',
+  'LOL'                            => 'League of Legends',
+  'MC'                             => 'Minecraft',
+  'TF2'                            => 'Team Fortress 2',
+  'OTHER_GAMES'                    => 'Do you have other games you enjoy playing?',
+  'FORMER_MEMBER'                  => 'Have you been a member of Christian Crew Gaming before?',
+  'MEMBER_OF_ANOTHER_CLAN'         => 'Are you currently a member of another clan?',
+  'CLAN_INFO'                      => 'If yes, please provide us with the name of the clan, website and/or Steam group (if any), and what games they primarily play',
+
+  'YOUR_FAITH'                     => 'Tell Us About Your Faith',
+  'BORN_AGAIN'                     => 'The phrase "born again" literally means "born from above" &mdash; a spiritual transformation. It means that because of Christ, the old sinner is dead and has been "Born Again", belonging to God. Are you a Born Again Christian (John 3:5, 2 Corinthians 5:17)?',
+  'NOT_SURE'                       => 'I\'m not sure',
+  'TESTIMONY'                      => 'The word testimony means an open declaration or profession of faith (Romans 10:9). It\'s a way to tell others how you came to know Christ, how He has changed you, or how much He has done for you. In your own words share your testimony',
+  'MINISTRY_INVOLVEMENT'           => 'Are you currently involved in any ministry work, either in your church, school, or community?',
+  'DENOMINATION'                   => 'What religion, faith, or denomination do you associate yourself with?',
+  'OTHER_RELIGION'                 => 'You\'ve chosen to apply to ACC which means you don\'t claim to be a Christian &mdash; do you associate youself with any religion or sets of beliefs?',
+
+  'ABOUT_YOU'                      => 'Tell Us About You',
+  'WHY_JOIN'                       => 'Why do you wish to join Christian Crew Gaming?',
+  'CONTRIBUTE'                     => 'Are there any areas in Christian Crew Gaming where you would like to contribute?',
+  'CONTRIBUTE_HELP_TEXT'           => 'Server admin, website, leadership, etc',
+  'KNOW_ANY_MEMBERS'               => 'Do you know anyone in real life that is a member of Christian Crew Gaming? If yes, please provide his or her username',
+  'AGREE'                          => 'Do you agree, without reservation, with the purpose of Christian Crew Gaming as stated in our Mission Statement?',
 
 ));
