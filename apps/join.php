184583daabe710403aef22c5422695ec9f937c47
Keith Brinks <keithbrinks@me.com>
2015-11-20 22:15:33 -0600

    Build join application body

--- apps/join.php
+++ apps/join.php
@@ -73,8 +73,70 @@ class join implements apps_interface {
     // Load the language file
     include($this->phpbb_root_path .'ext/thechristiancrew/apps/language/en/'. $this->app .'_lang.php');
 
-    $body = '[b]'. $lang['INGAME_NAME'] .'[/b]
-    '. $this->request->variable('ingame_name', '');
+    $body = '[b]'. $lang['APP_TYPE'] .'[/b]
+    '. $this->request->variable('app_type', '') .'
+
+    [size=120][u][b]'. $lang['PERSONAL_INFORMATION'] .'[/b][/u][/size]
+    [b][i]'. $lang['INGAME_NAME'] .':[/i][/b] '. $this->request->variable('ingame_name', '') .'
+    [b][i]'. $lang['REAL_NAME'] .':[/i][/b] '. $this->request->variable('real_name', '') .'
+    [b][i]'. $lang['AGE'] .':[/i][/b] '. $this->request->variable('age', '') .'
+    [b][i]'. $lang['LOCATION'] .':[/i][/b] '. $this->request->variable('location', '') .'
+    [b][i]'. $lang['STEAM_COMMUNITY_LINK'] .':[/i][/b] '. $this->request->variable('steam_community_link', '') .'
+
+    [b][i]'. $lang['FAVORITE_FISH_SERVERS'] .'[/i][/b]
+    '. implode(', ', $this->request->variable('favorite_fish_servers', array('1'=>''))) .'
+
+    [b][i]'. $lang['OTHER_GAMES'] .'[/i][/b]
+    '. $this->request->variable('other_games', '') .'
+
+    [b][i]'. $lang['FORMER_MEMBER'] .'[/i][/b]
+    '. $this->request->variable('former_member', '') .'
+
+    [b][i]'. $lang['MEMBER_OF_ANOTHER_CLAN'] .'[/i][/b]
+    '. $this->request->variable('member_of_another_clan', '') .'
+
+    [b][i]'. $lang['CLAN_INFO'] .'[/i][/b]
+    '. $this->request->variable('clan_info', '') .'
+
+    [size=120][u][b]'. $lang['YOUR_FAITH'] .'[/b][/u][/size]
+    '; // Line break
+
+    if ($this->request->variable('app_type', '') == 'CC') {
+
+      $body .= '[b][i]'. $lang['BORN_AGAIN'] .'[/i][/b]
+      '. $this->request->variable('born_again', '') .'
+
+      [b][i]'. $lang['TESTIMONY'] .'[/i][/b]
+      '. $this->request->variable('testimony', '') .'
+
+      [b][i]'. $lang['MINISTRY_INVOLVEMENT'] .'[/i][/b]
+      '. $this->request->variable('ministry_involvement', '') .'
+
+      [b][i]'. $lang['DENOMINATION'] .'[/i][/b]
+      '. $this->request->variable('denomination', '') .'
+      '; // Line break
+
+    } else {
+
+      $body .= '[b][i]'. $lang['OTHER_RELIGION'] .'[/i][/b]
+      '. $this->request->variable('other_religion', '') .'
+      '; // Line break
+
+    }
+
+    $body .= '
+    [size=120][u][b]'. $lang['ABOUT_YOU'] .'[/b][/u][/size]
+    [b][i]'. $lang['WHY_JOIN'] .'[/i][/b]
+    '. $this->request->variable('why_join', '') .'
+
+    [b][i]'. $lang['CONTRIBUTE'] .'[/i][/b]
+    '. $this->request->variable('contribute', '') .'
+
+    [b][i]'. $lang['KNOW_ANY_MEMBERS'] .'[/i][/b]
+    '. $this->request->variable('know_any_members', '') .'
+
+    [b][i]'. $lang['AGREE'] .'[/i][/b]
+    '. $this->request->variable('agree', '');
 
     return $body;
 
